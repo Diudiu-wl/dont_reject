3b1a42e95e08faf470deb248b106d0b982c01fe0
Lan Wang <lan.lovesmaracuja@gmail.com>
2025-12-28 00:37:51 +0100

    .

--- src/App.js
+++ src/App.js
@@ -8,13 +8,12 @@ function App() {
         <h1 className="title">欢迎下载表白小软件</h1>
         <p className="subtitle">拿去发给你的crush吧~</p>
 
-        <a
-          href="/downloads/dont_reject.exe"
-          download
-          className="download-btn"
-        >
-          下载
-        </a>
+        <button
+            className="download-btn"
+            onClick={() => window.location.href = `${process.env.PUBLIC_URL}/downloads/dont_reject.exe`}
+          >
+            下载
+          </button>
       </div>
     </div>
   );
